e390928ef321aab1e9e08ed6aa324f7008cb7a0e
mattddonovan <mattddonovan@gmail.com>
2019-01-03 07:02:33 -0500

    More progress, functional piece complete

--- src/components/CSVAnalyzer.js
+++ src/components/CSVAnalyzer.js
@@ -1,31 +1,45 @@
 import React, { Component } from 'react';
 import PapaParse from 'papaparse';
+import { uniqWith } from 'lodash';
+
+// Levenshtein implementation from https://github.com/hiddentao/fast-levenshtein#readme
 const levenshtein = require('fast-levenshtein');
 
-
+/**
+ * Simple component for taking in and processing a CSV
+ */
 class CSVAnalyzer extends Component {
-
   constructor(props) { 
     super(props); 
 
-    this.state = {
-      
-    }
+    this.data = [];
+    this.dataMap = [];
+    this.uniques = [];
+    this.duplicates = [];
 
     // Bind functions
     this.onFileLoaded = this.onFileLoaded.bind(this);
     this.findDuplicates = this.findDuplicates.bind(this);
+    this.isUnique = this.isUnique.bind(this);
   }
   
-
-  handleFileChange = e => {
+  /**
+   * Handle a CSV upload 
+   * @param {Object} FileList 
+   */
+  handleFileChange = (input) => {
+    // Instantiate a FileReader to load our CSV file into
     let reader = new FileReader();
-    const filename = e.target.files[0].name;
+    const filename = input.target.files[0].name;
+
+    // PapaParse-specific config
     const parserOptions = {
       delimiter: ",",
       header: true
     }
 
+    // Handle the onload event for our reader and pass
+    // the payload off to our callback
     reader.onload = event => {
       const csvData = PapaParse.parse(
         event.target.result,
@@ -33,84 +47,100 @@ class CSVAnalyzer extends Component {
       );
       this.onFileLoaded(csvData.data, filename);
     }
-    reader.readAsText(e.target.files[0]);
+
+    // Start streaming our file
+    reader.readAsText(input.target.files[0]);
   }
 
+  // Callback for CSV upload
+  onFileLoaded = (data, filename) => {
+    console.log('Successfully loaded ' + filename);
+    this.data = data;
+    this.findDuplicates(this.data);
+  }
+
+
+  /**
+   * Find duplicate and unique values within a given data set
+   * @param {Object[]} data
+   * @public
+   */
   findDuplicates = (data) => {
-    let seen = {};
-    let dupes = [];
 
-    // Sort data alphabetically (using first_name)
-    data.sort((a, b) => a.first_name.localeCompare(b.first_name));
-
-    // Use 'reduce' to generate unique key using first and last name.
-    // Entries sharing the same key will be grouped together, allowing
-    // us to find the easy duplicates.
-    let groupedData = data.reduce((res, val) => {
-      let key = val.first_name + '-' + val.last_name;
-      res[key] = res[key] || [];
-      res[key].push(val);
-      return res;
-    },{});
-
-    // Make a second pass iterating through the keys to catch potential
-    // outlier dupes (typos, etc) using the Levenshtein distance algorithm
-    Object.keys(groupedData).forEach((key, index, arr) => {
-      
+    /**
+     * Create a new collection on this class, adding an additional 
+     * for 'key' property generated from the 'first_name' and 'last_name'
+     * fields. This key will be used when determining uniquity below
+    */
+    this.dataMap = data.map((val) => {
+      let key = val['first_name'] + '-' + val['last_name'];
+      val['key'] = key;
+      return val;
     });
 
-    console.log(groupedData);
-
-   
-
-    //console.log(groupedData);
-
-    //console.log(levenshtein.get('Jacqueline-Ilchenko', 'Jacquelyn-Ilchenko'))
-
-    /*
-    var result = Object.values(data.reduce((, row) => {
-      let key = row['first_name'] + '-' + row['last_name'];
-      entries[key] = entries[key] || [];
-      entries[key].push(row);
-      return entries;
-    },)).reduce((c, v) => { 
-      console.log(v)
-      return v.length > 1 ? c.concat(v) : c;
-    }, []);
-
-
-*/
-
-    //console.log(result);
-    console.log(dupes);
-
+    // Sort data alphabetically off key value
+    this.dataMap.sort((a, b) => a['key'].localeCompare(b['key']));
     
+    /** 
+     * Find the unique objects in the collection, see class method
+     * isUnique for implementation details. Note: Using lodash's 'uniqWith'
+     * function to simplify object property comparison. 
+     */
+    this.uniques = uniqWith(this.dataMap, this.isUnique);
 
-    
-    
-    //.reduce((c, v) => v.length > 0 ? c.concat(v) : c, []);
+    /** 
+     * Find the duplicates by finding the difference between our
+     * original data collection and our uniques
+     */
+    this.duplicates = this.dataMap.filter((val) => {
+      return !this.uniques.includes(val);
+    });
 
-    //console.log(data);
+    this.printResults();
   }
 
-  // Callback
-  onFileLoaded = (data, filename) => {
-    console.log(filename + ' successfully loaded');
-    this.findDuplicates(data);
+  /** 
+   * Comparator used in determining uniquity between two
+   * keys on separate objects.  It uses the levenshtein distance
+   * algorithm to allow for detection of edge case duplicates (typos, etc)
+   * @param {Object} a
+   * @param {Object} b
+   * @public
+  */
+  isUnique = (a, b) => {
+    let dist = levenshtein.get(a['key'], b['key']);
+    return (dist >= 0 && dist <= 3);
+  }
+
+  /** 
+   * Print results to console
+   * 
+   */
+  printResults() {
+    console.log('Potential Duplicates: (' + this.duplicates.length + ')');
+    console.log('..........');
+    this.duplicates.forEach(val => console.log(JSON.stringify(val)));
+    console.log('..........');
+    console.log('Non Duplicates: (' + this.uniques.length + ')');
+    console.log('..........');
+    this.uniques.forEach(val => console.log(JSON.stringify(val)));
   }
 
 
+  // Render function
   render () {
     return (
-      <div className="csv-analyzer">
-        <label htmlFor="csvFileInput">Select a CSV</label>
-        <input
-          className="css-file-input"
-          type="file"
-          id="csvFileInput"
-          accept=".csv, text/csv"
-          onChange={e => this.handleFileChange(e)}
-        />
+      <div className="container">
+        <div className="csv-analyzer">
+          <label htmlFor="csvFileInput">Select a CSV</label>
+          <input
+            className="css-file-input"
+            type="file"
+            id="csvFileInput"
+            accept=".csv, text/csv"
+            onChange={e => this.handleFileChange(e)}
+          />
+        </div>
       </div>
     )
   }
